27b83dd862a9213d344ce304cbf1206e47f7d86e
gaoyun <gaoyun@putaogame.com>
2018-02-09 12:06:57 +0000

    first

---
+++ library/imports/13/13172866-6913-4a8c-94ba-f7963c1d628a.js
@@ -0,0 +1,62 @@
+"use strict";
+cc._RF.push(module, '13172hmaRNKjJS695Y8HWKK', 'Game');
+// Script/Game.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start: function start() {
+        var hero = this.getComponent("hero");
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
+
+
+    onKeyDown: function onKeyDown(event) {
+        switch (event.keyCode) {
+            case cc.KEY.a:
+
+            case cc.KEY.d:
+        }
+    },
+
+    onKeyUp: function onKeyUp(event) {},
+
+    update: function update(dt) {}
+});
+
+cc._RF.pop();
